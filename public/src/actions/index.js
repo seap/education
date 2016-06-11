e8c554626b6c7c956f8e3cf92549eac69f33ad37
seayang <ykudos@gmail.com>
2016-06-11 11:10:34 +0800

    update

--- public/src/actions/index.js
+++ public/src/actions/index.js
@@ -79,7 +79,7 @@ export function wxConfig() {
       let response = await fetch(`/wechat/signature?url=${encodeURIComponent(window.location.href)}`);
       let json = await response.json();
       wx.config({
-        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: 'wx95013eaa68c846c7', // 必填，公众号的唯一标识
         timestamp: json.timestamp , // 必填，生成签名的时间戳
         nonceStr: json.noncestr, // 必填，生成签名的随机串
