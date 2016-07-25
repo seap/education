fcfe2f9bf0c0fbb8e37340fa202e2fcc66ae5d39
seayang <ykudos@gmail.com>
2016-07-26 05:53:05 +0800

    modifiy appId in action

--- public/src/actions/index.js
+++ public/src/actions/index.js
@@ -155,7 +155,7 @@ export function wxConfig() {
       let json = await response.json();
       wx.config({
         debug: __DEVELOPMENT__, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: 'wx95013eaa68c846c7', // 必填，公众号的唯一标识
+        appId: 'wx959feca69d5ba361', // 必填，公众号的唯一标识
         timestamp: json.timestamp , // 必填，生成签名的时间戳
         nonceStr: json.noncestr, // 必填，生成签名的随机串
         signature: json.signature,// 必填，签名，见附录1
